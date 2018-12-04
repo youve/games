1d6f6b053a766ca3735de61c07ca904a69bdc933
Youve <minny.molly@gmx.com>
2018-12-04 14:20:42 +0100

    Disallow negative indices for mistakes near beginning of line

--- typing.py
+++ typing.py
@@ -38,9 +38,12 @@ def typeFortune():
             text[i] = text[i] + " "
         for j in range(len(fortune[i])):
             if fortune[i][j] != text[i][j]:
-                mistake = [fortune[i][j-5:j+5] + '\n']
+                if j-5 > 0:
+                    mistake = [fortune[i][j-5:j+5] + '\n'] #surrounding context of the error
+                else: #mistake is near the beginning
+                    mistake = [fortune[i][:j+5] + '\n']
                 mistake.append(text[i][j-5:j])
-                mistake.append('\033[1m' + text[i][j] + '\033[0m')
+                mistake.append('\033[4m' + text[i][j] + '\033[0m') # underline the error
                 mistake.append(text[i][j+1:j+5])
                 errors.append(''.join(mistake))
     text = '\n'.join(text) 
@@ -51,6 +54,7 @@ if input('Type your [O]wn text or an [A]ssigned one? ').upper() == 'O':
 else:
     text, errors, seconds = typeFortune()
 
+#get stats ready for printing
 colWidth = 1
 characters = str(len(text))
 CPM = str(round(len(text)*60/(seconds)))
@@ -61,16 +65,17 @@ ACPM = str(round((len(text) - len(errors))*60/seconds))
 AWPM = str(round((len(text) - len(errors))*60/5/seconds))
 seconds = str(seconds)
 
+# align columns
 for item in (characters, seconds, CPM, WPM, errorRate, errorCount, ACPM, AWPM):
     if len(item) > colWidth:
         colWidth = len(item) + 2
 
+#Print errors
 print(f'Errors: {errorCount}:')
 for error in errors:
     print(error,'\n')
 
+#Print stats
 print('Statistics:')
 print(f'Characters: {characters.rjust(colWidth)}\tSeconds: {seconds.rjust(colWidth)}\tCPM:  {CPM.rjust(colWidth)}\tWPM:  {WPM.rjust(colWidth)}')
 print(f'Error rate: {errorRate.rjust(colWidth)}%\tErrors:  {errorCount.rjust(colWidth)}\tACPM: {ACPM.rjust(colWidth)}\tAWPM: {AWPM.rjust(colWidth)}')
-
-#todo: adjusted WPM/CPM for errors
